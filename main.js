4367bd63cd73992c67441e78eadd7bbbd5765247
gaohanghang <1341947277@qq.com>
2020-03-03 20:57:30 +0800

    修改默认窗口大小

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1350,
+    height: 880,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
